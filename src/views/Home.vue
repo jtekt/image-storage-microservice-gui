5093d0287087732c9e65ab3f3394a9207d3e738b
Maxime Moreillon <m.moreillon@gmail.com>
2020-07-01 23:22:55 +0000

    minor cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,6 @@
 <template>
   <div class="home">
     <h1>Storage MS GUI</h1>
-    <h2>System map</h2>
-    <div class="image_wrapper">
-      <img src="@/assets/system_map.png" alt="">
-    </div>
-    <h2>Source code</h2>
-    <ul>
-      <li>
-        <a href="http://172.16.98.151:8081/tokushima_k3s/watch_service">Watch microservice</a>
-      </li>
-      <li>
-        <a href="http://172.16.98.151:8081/tokushima_k3s/storage_microservice">Storage microservice</a>
-      </li>
-      <li>
-        <a href="http://172.16.98.151:8081/mike_k8s/tokushima_storage_microservice_front">Storage microservice UI</a>
-      </li>
-      <li>
-        <a href="http://172.16.98.151:8081/tokushima_k3s/tokushima_tf2.0">AI classifier</a>
-      </li>
-      <li>
-        <a href="http://172.16.98.151:8081/kojima/tokushima_result_viewer">Analytics microservice</a>
-      </li>
-
-    </ul>
-
-
   </div>
 </template>
 
