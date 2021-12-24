aed801638a6571e1fb35fcc44d27b7b69a91aca4
Maxime Moreillon <m.moreillon@gmail.com>
2021-12-24 17:24:42 +0900

    template

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,57 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="#444444"
-      dark >
-      <v-toolbar-title>Image storage service GUI</v-toolbar-title>
+  <AppTemplate
+    :options="options">
 
-      <v-spacer></v-spacer>
+    <template v-slot:nav>
+      <v-list
+        dense
+        nav >
+        <v-list-item
+          v-for="(item, index) in nav"
+          :key="`nav_item_${index}`"
+          :to="item.to"
+          exact>
+          <v-list-item-icon>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-item-icon>
 
-      <v-btn
-        text
-        dark
-        :to="{name:'about'}">
-        <v-icon>mdi-information-outline</v-icon>
-        <span class="ml-1">About</span>
-      </v-btn>
+          <v-list-item-content>
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </template>
 
-    </v-app-bar>
-
-    <v-main class="grey lighten-4">
-      <AuthenticationWall
-        v-if="use_auth"
-        :options="auth_options">
-        <v-container fluid>
-          <router-view/>
-        </v-container>
-      </AuthenticationWall>
-
-      <v-container fluid v-else>
-        <router-view/>
-      </v-container>
-
-    </v-main>
-
-  </v-app>
+  </AppTemplate>
 </template>
 
 <script>
-import AuthenticationWall from '@moreillon/vue_authentication_wall_vuetify'
-
+import AppTemplate from '@moreillon/vue_application_template_vuetify'
 export default {
   name: 'App',
+
   components: {
-    AuthenticationWall
+    AppTemplate
   },
-  data: () => ({
-    use_auth: process.env.VUE_APP_LOGIN_URL && process.env.VUE_APP_IDENTIFICATION_URL,
-    auth_options: {
-      title: 'Image storage service GUI',
-      login_url: process.env.VUE_APP_LOGIN_URL,
-      identification_url: process.env.VUE_APP_IDENTIFICATION_URL,
-    },
-    nav: [
-      {title: 'My profile', to: {name: 'employee', params: {employee_id: 'self'} }, icon: 'mdi-account'},
-      {title: 'Employees', to: {name: 'employees' }, icon: 'mdi-account-search'},
-      {title: 'Groups', to: {name: 'groups' }, icon: 'mdi-account-multiple'},
-      {title: 'Workplaces', to: {name: 'workplaces' }, icon: 'mdi-office-building'},
-      {title: 'About', to: {name: 'about'}, icon: 'mdi-information-outline'},
-    ]
-  }),
-}
+
+  data() {
+    return {
+      options: {
+        title: "Image storage service",
+        authenticate: true,
+        login_url: process.env.VUE_APP_LOGIN_URL,
+        identification_url: process.env.VUE_APP_IDENTIFICATION_URL,
+      },
+      nav: [
+        {title: 'Collections', to: {name: 'collections'}, icon: 'mdi-home'},
+        {title: 'About', to: {name: 'about'}, icon: 'mdi-information-outline'},
+      ]
+    }
+
+  },
+
+  computed: {
+
+  }
+};
 </script>
