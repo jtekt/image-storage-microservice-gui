aa29c0baff73f14138b13dec803779feb03f6061
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-20 12:10:36 +0900

    export according to filters

--- src/components/ExportButton.vue
+++ src/components/ExportButton.vue
@@ -1,30 +1,32 @@
 <template>
-    <v-btn text block @click="export_collection()">
-        <v-icon left>mdi-download</v-icon>
-        <span>Export</span>
-    </v-btn>
+  <v-btn text block @click="export_collection()">
+    <v-icon left>mdi-download</v-icon>
+    <span>Export</span>
+  </v-btn>
 </template>
 
 <script>
 export default {
-    name: 'ExportButton',
-    data() {
-        return {
+  name: "ExportButton",
+  data() {
+    return {}
+  },
+  methods: {
+    export_collection() {
+      const { limit, skip, sort, order, ...params } = this.query // eslint-disable-line no-unused-vars
+      const url = new URL(`${process.env.VUE_APP_IMAGE_STORAGE_API_URL}/export`)
 
-        }
+      Object.keys(params).forEach((key) => {
+        url.searchParams.append(key, params[key])
+      })
+
+      window.open(url.toString(), "_blank")
     },
-    methods: {
-        export_collection() {
-            // TODO: Export according to filters
-            const url = `${process.env.VUE_APP_IMAGE_STORAGE_API_URL}/export`
-            window.open(url, '_blank')
-        },
-
-
+  },
+  computed: {
+    query() {
+      return this.$route.query
     },
-    computed: {
-
-    }
-
+  },
 }
 </script>
