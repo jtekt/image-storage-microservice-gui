041eefa09f3581b3fc29a6d5c0d54e67f48ba24a
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-20 13:51:47 +0900

    date query in UTC

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,51 +1,61 @@
 <template>
-    <!-- :return-value.sync="date" -->
-    <v-menu ref="menu" v-model="menu" :close-on-content-click="false" transition="scale-transition" offset-y
-        min-width="auto">
+  <!-- :return-value.sync="date" -->
+  <v-menu
+    ref="menu"
+    v-model="menu"
+    :close-on-content-click="false"
+    transition="scale-transition"
+    offset-y
+    min-width="auto"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-text-field
+        v-model="date"
+        :label="label"
+        prepend-icon="mdi-calendar"
+        readonly
+        v-bind="attrs"
+        v-on="on"
+        clearable
+      />
+    </template>
 
-        <template v-slot:activator="{ on, attrs }">
-            <v-text-field v-model="date" :label="label" prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on"
-                clearable />
-        </template>
+    <v-date-picker v-model="date" no-title scrollable>
+      <v-spacer />
 
-
-        <v-date-picker v-model="date" no-title scrollable>
-            <v-spacer />
-
-            <v-btn text color="primary" @click="menu = false">
-                Close
-            </v-btn>
-        </v-date-picker>
-
-
-    </v-menu>
+      <v-btn text color="primary" @click="menu = false"> Close </v-btn>
+    </v-date-picker>
+  </v-menu>
 </template>
 
 <script>
 export default {
-    name: 'DatePickerMenu',
-    props: {
-        label: String,
-        value: String,
-    },
-    data() {
-        return {
-            menu: false,
-        }
-    },
-    methods: {
-
-    },
-    computed: {
-        date: {
-            get() {
-                return this.value
-            },
-            set(newVal) {
-                this.$emit('input', newVal)
-            }
-        }
+  name: "DatePickerMenu",
+  props: {
+    label: String,
+    value: String,
+  },
+  data() {
+    return {
+      menu: false,
     }
-
+  },
+  methods: {},
+  computed: {
+    date: {
+      get() {
+        // TODO: find nicer way
+        if (!this.value) return null
+        const date = new Date(this.value)
+        const year = date.getFullYear()
+        const month = (date.getMonth() + 1).toString().padStart(2, "0")
+        const day = date.getDate().toString().padStart(2, "0")
+        return `${year}-${month}-${day}`
+      },
+      set(newVal) {
+        this.$emit("input", new Date(newVal).toISOString())
+      },
+    },
+  },
 }
 </script>
