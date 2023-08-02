97564f256317035886c02d9c38053211246289e8
Maxime Moreillon <m.moreillon@gmail.com>
2023-08-03 07:35:06 +0900

    removed back button

--- src/views/Image.vue
+++ src/views/Image.vue
@@ -1,145 +1,133 @@
 <template>
-  <v-card
-    :loading="loading">
+  <v-card :loading="loading">
     <v-toolbar flat>
-
-      <v-row align='center'>
-        <v-col cols="auto">
-          <v-btn
-            icon
-            @click="$router.go(-1)">
-            <v-icon>mdi-arrow-left</v-icon>
-          </v-btn>
-        </v-col>
+      <v-row align="center">
         <v-col>
-          <v-toolbar-title>{{$t('Image')}}</v-toolbar-title>
+          <v-toolbar-title>{{ $t("Image") }}</v-toolbar-title>
         </v-col>
-        <v-spacer/>
+        <v-spacer />
         <v-col cols="auto">
           <v-btn
             :loading="deleting"
             color="#c00000"
             icon
-            @click="delete_item()">
+            @click="delete_item()"
+          >
             <v-icon>mdi-delete</v-icon>
           </v-btn>
         </v-col>
       </v-row>
     </v-toolbar>
-    <v-divider/>
 
-      <v-card-text v-if="item">
-        <v-row justify="center">
-          <v-col cols="auto">
-            <img class="item_image" :src="image_src" />
-          </v-col>
-        </v-row>
-        <v-list>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-subtitle>ID</v-list-item-subtitle>
-              <v-list-item-title>{{item._id}}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-subtitle>Time</v-list-item-subtitle>
-              <v-list-item-title >{{time_formatted}}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-subtitle>File name</v-list-item-subtitle>
-              <v-list-item-title>{{item.file}}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-        <v-list>
-          <v-list-item
-            v-for="(value, key) in item.data"
-            :key="key">
-            <v-list-item-content>
-              <v-list-item-subtitle>{{key}}</v-list-item-subtitle>
-              <v-list-item-title>
-                <pre>{{format_metadata(value)}}</pre>
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-card-text>
-
-
+    <v-card-text v-if="item">
+      <v-row justify="center">
+        <v-col cols="auto">
+          <img class="item_image" :src="image_src" />
+        </v-col>
+      </v-row>
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-subtitle>ID</v-list-item-subtitle>
+            <v-list-item-title>{{ item._id }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-subtitle>Time</v-list-item-subtitle>
+            <v-list-item-title>{{ time_formatted }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-subtitle>File name</v-list-item-subtitle>
+            <v-list-item-title>{{ item.file }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-list>
+        <v-list-item v-for="(value, key) in item.data" :key="key">
+          <v-list-item-content>
+            <v-list-item-subtitle>{{ key }}</v-list-item-subtitle>
+            <v-list-item-title>
+              <pre>{{ format_metadata(value) }}</pre>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
   </v-card>
-
 </template>
 
 <script>
 export default {
-  name: 'Images',
-  data(){
+  name: "Images",
+  data() {
     return {
       loading: false,
       deleting: false,
       item: null,
     }
   },
-  mounted(){
+  mounted() {
     this.get_item()
   },
   computed: {
     image_id() {
       return this.$route.params._id
     },
-    image_src(){
+    image_src() {
       return `${process.env.VUE_APP_IMAGE_STORAGE_API_URL}/images/${this.image_id}/image`
     },
-    time_formatted(){
+    time_formatted() {
       const date = new Date(this.item.time)
-      return date.toLocaleString('ja-JP')
-    }
-
+      return date.toLocaleString("ja-JP")
+    },
   },
   methods: {
-    get_item(){
+    get_item() {
       this.loading = true
       this.item = null
       const route = `/images/${this.image_id}`
-      this.axios.get(route)
-      .then( ({data}) => {
-        this.item = data
-       })
-      .catch( (error) => {
-        alert('Failed to query data')
-        console.error(error)
-      })
-      .finally(() => {
-        this.loading = false
-      })
+      this.axios
+        .get(route)
+        .then(({ data }) => {
+          this.item = data
+        })
+        .catch((error) => {
+          alert("Failed to query data")
+          console.error(error)
+        })
+        .finally(() => {
+          this.loading = false
+        })
     },
-    delete_item(){
-      if(!confirm(`Delete image ${this.image_id}?`)) return
+    delete_item() {
+      if (!confirm(`Delete image ${this.image_id}?`)) return
       this.deleting = true
       const route = `/images/${this.image_id}`
-      this.axios.delete(route)
-      .then( () => { this.$router.push({name: 'images'}) })
-      .catch( (error) => {
-        alert('Failed to delete data')
-        console.error(error)
-      })
-      .finally(() => {
-        this.deleting = false
-      })
+      this.axios
+        .delete(route)
+        .then(() => {
+          this.$router.push({ name: "images" })
+        })
+        .catch((error) => {
+          alert("Failed to delete data")
+          console.error(error)
+        })
+        .finally(() => {
+          this.deleting = false
+        })
     },
+
     format_metadata(data) {
       try {
         return JSON.stringify(data, null, 2)
       } catch (error) {
         return data
       }
-    }
-
+    },
   },
-
 }
 </script>
 
@@ -147,5 +135,4 @@ export default {
 .item_image {
   max-height: 60vh;
 }
-
 </style>
