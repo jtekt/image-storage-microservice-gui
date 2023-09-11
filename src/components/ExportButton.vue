e1eeff9ee9ac358b7c0cffcabd086e80ea2a12f4
Maxime Moreillon <m.moreillon@gmail.com>
2023-09-11 09:31:07 +0900

    export when relative apu urls

--- src/components/ExportButton.vue
+++ src/components/ExportButton.vue
@@ -1,32 +1,33 @@
 <template>
-  <v-btn text block @click="export_collection()">
-    <v-icon left>mdi-download</v-icon>
-    <span>Export</span>
-  </v-btn>
+    <v-btn text block @click="export_collection()">
+        <v-icon left>mdi-download</v-icon>
+        <span>Export</span>
+    </v-btn>
 </template>
 
 <script>
 export default {
-  name: "ExportButton",
-  data() {
-    return {}
-  },
-  methods: {
-    export_collection() {
-      const { limit, skip, sort, order, ...params } = this.query // eslint-disable-line no-unused-vars
-      const url = new URL(`${process.env.VUE_APP_IMAGE_STORAGE_API_URL}/export`)
-
-      Object.keys(params).forEach((key) => {
-        url.searchParams.append(key, params[key])
-      })
-
-      window.open(url.toString(), "_blank")
+    name: 'ExportButton',
+    data() {
+        return {}
     },
-  },
-  computed: {
-    query() {
-      return this.$route.query
+    methods: {
+        export_collection() {
+            const { limit, skip, sort, order, ...params } = this.query // eslint-disable-line no-unused-vars
+
+            const urlSearch = Object.keys(params)
+                .map((key) => `${key}=${params[key]}`)
+                .join('&')
+
+            const url = `${process.env.VUE_APP_IMAGE_STORAGE_API_URL}/export?${urlSearch}`
+
+            window.open(url, '_blank')
+        },
     },
-  },
+    computed: {
+        query() {
+            return this.$route.query
+        },
+    },
 }
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,74 +1,51 @@
 <template>
-  <v-card>
-    <v-toolbar flat>
-      <v-btn
-        icon
-        exact
-        :to="{name: 'images'}">
-        <v-icon>mdi-arrow-left</v-icon>
-      </v-btn>
-      <v-toolbar-title>Image storage service GUI</v-toolbar-title>
-    </v-toolbar>
+    <v-card>
+        <v-toolbar flat>
+            <v-btn icon exact :to="{ name: 'images' }">
+                <v-icon>mdi-arrow-left</v-icon>
+            </v-btn>
+            <v-toolbar-title>Image storage service GUI</v-toolbar-title>
+        </v-toolbar>
 
-    <v-card-text>
-      <p>Developed and maintainted by Maxime Moreillon, JTEKT Corporation</p>
-      <v-data-table
-        hide-default-footer
-        :itemsPerPage="-1"
-        :headers="headers"
-        :items="services"/>
-    </v-card-text>
-
-
-  </v-card>
+        <v-card-text>
+            <p>v {{ version }}</p>
+            <p>
+                Developed and maintainted by Maxime Moreillon, JTEKT Corporation
+            </p>
+            <v-data-table
+                hide-default-footer
+                :itemsPerPage="-1"
+                :headers="headers"
+                :items="services"
+            />
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
-import pjson from '@/../package.json'
+import { version } from '@/../package.json'
 export default {
-  name: 'About',
-  data () {
-    return {
-      headers: [
-        {text: 'Service', value: "name"},
-        {text: 'Version', value: "version"},
-        {text: 'URL', value: "url"},
-
-      ],
-      services: [
-        {
-          name: 'Image storage GUI',
-          url: window.location.origin,
-          version: pjson.version
-        },
-        {
-          name: 'Image storage Back-end',
-          url: process.env.VUE_APP_IMAGE_STORAGE_API_URL,
-          version: null
-        },
-      ],
-    }
-  },
-  mounted () {
-    this.get_services_version()
-  },
-  methods: {
-
-    get_services_version () {
-      this.services.forEach((service) => {
-        if (service.version) return
-        service.version = 'Connecting...'
-        this.axios.get(service.url)
-          .then(({ data }) => { service.version = data.version })
-          .catch(() => { service.version = 'Unable to connect' })
-      })
-    }
-  }
-
+    name: 'About',
+    data() {
+        return {
+            version,
+            headers: [
+                { text: 'Service', value: 'name' },
+                { text: 'Version', value: 'version' },
+            ],
+            services: [
+                {
+                    name: 'Image storage GUI',
+                    url: window.location.origin,
+                },
+                {
+                    name: 'Image storage Back-end',
+                    url: process.env.VUE_APP_IMAGE_STORAGE_API_URL,
+                },
+            ],
+        }
+    },
 }
 </script>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
