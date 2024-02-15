b603ac745544aa36483440eb9bc0ff5b7db39da4
Leah Joy Ishiguro <leah_ishiguro@jtekt.co.jp>
2024-02-15 10:46:11 +0900

    Separate component for image data editor

---
+++ src/components/ImageDataEditor.vue
@@ -0,0 +1,173 @@
+<template>
+    <v-card elevation="0" outlined v-if="json">
+        <v-toolbar flat>
+            <v-row align="center">
+                <v-col>
+                    <v-toolbar-title class="font-weight-medium">
+                        {{ title }}</v-toolbar-title
+                    >
+                </v-col>
+                <v-spacer />
+                <v-col cols="auto">
+                    <div v-if="edit_mode">
+                        <v-tooltip bottom>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn
+                                    icon
+                                    v-bind="attrs"
+                                    v-on="on"
+                                    color="error"
+                                    @click.stop="cancel_edit()"
+                                >
+                                    <v-icon> mdi-close-box </v-icon>
+                                </v-btn>
+                            </template>
+                            <div class="text-center">Cancel Changes</div>
+                        </v-tooltip>
+                        <v-tooltip bottom>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn
+                                    icon
+                                    v-bind="attrs"
+                                    v-on="on"
+                                    color="success"
+                                    :disabled="!isJsonValid || !made_changes"
+                                    @click.stop="save_data()"
+                                >
+                                    <v-icon> mdi-content-save </v-icon>
+                                </v-btn>
+                            </template>
+                            <div class="text-center">Save Changes</div>
+                        </v-tooltip>
+                    </div>
+                    <v-tooltip bottom v-else>
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-btn
+                                icon
+                                v-bind="attrs"
+                                v-on="on"
+                                color="secondary"
+                                @click.stop="start_edit()"
+                            >
+                                <v-icon> mdi-lead-pencil </v-icon>
+                            </v-btn>
+                        </template>
+                        <div class="text-center">Edit image properties</div>
+                    </v-tooltip>
+                </v-col>
+            </v-row>
+        </v-toolbar>
+        <v-card-text>
+            <div v-if="edit_mode" class="pt-5">
+                <v-textarea
+                    filled
+                    no-resize
+                    :rows="textarea_row"
+                    :row-height="textarea_row"
+                    v-model="data_string"
+                    :error="!isJsonValid"
+                    :error-messages="!isJsonValid ? 'Invalid JSON' : ''"
+                />
+            </div>
+            <div v-else>
+                <v-row>
+                    <v-col>
+                        <v-list>
+                            <v-list-item
+                                v-for="(value, key) in json"
+                                :key="key"
+                            >
+                                <v-list-item-content>
+                                    <v-list-item-subtitle
+                                        class="text-button font-weight-medium"
+                                        >{{ key }}</v-list-item-subtitle
+                                    >
+                                    <v-list-item-title>
+                                        <pre>{{ format_metadata(value) }}</pre>
+                                    </v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list>
+                    </v-col>
+                </v-row>
+            </div>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'ImageDataEditor',
+    data() {
+        return {
+            edit_mode: false,
+            data_string: '',
+        }
+    },
+    props: {
+        title: {
+            type: String,
+            default: '',
+        },
+        json: {
+            type: [Object, Array],
+            default: null,
+        },
+        textarea_row: {
+            type: Number,
+            default: 10, // Default value for the prop
+        },
+    },
+    methods: {
+        format_metadata(data) {
+            try {
+                return JSON.stringify(data, null, 2)
+            } catch (error) {
+                return data
+            }
+        },
+        save_data() {
+            if (!this.isJsonValid) return
+            if (!confirm('Save changes?')) return
+            let data = JSON.parse(this.data_string)
+            this.$emit('save-data', data)
+            this.reset_changes()
+        },
+        start_edit() {
+            this.edit_mode = true
+            if (this.json) this.data_string = this.format_metadata(this.json)
+        },
+        cancel_edit() {
+            if (!confirm('Abandon changes?')) return
+            this.reset_changes()
+        },
+        reset_changes() {
+            this.edit_mode = false
+            this.data_string = ''
+        },
+    },
+    watch: {
+        data_string(newVal) {
+            if (newVal.trim() === '') {
+                this.data_string = '{}'
+            }
+        },
+    },
+    computed: {
+        isJsonValid() {
+            try {
+                JSON.parse(this.data_string)
+            } catch (e) {
+                return false
+            }
+            return true
+        },
+        made_changes() {
+            return (
+                JSON.stringify(this.json) !==
+                JSON.stringify(JSON.parse(this.data_string))
+            )
+        },
+    },
+}
+</script>
--- src/views/Image.vue
+++ src/views/Image.vue
@@ -81,131 +81,12 @@
             </v-row>
             <v-row>
                 <v-col>
-                    <v-card elevation="0" outlined>
-                        <v-toolbar flat>
-                            <v-row align="center">
-                                <v-col>
-                                    <v-toolbar-title class="font-weight-medium">
-                                        Editable Image Data</v-toolbar-title
-                                    >
-                                </v-col>
-                                <v-spacer />
-                                <v-col cols="auto">
-                                    <div v-if="edit_mode">
-                                        <v-tooltip bottom>
-                                            <template
-                                                v-slot:activator="{ on, attrs }"
-                                            >
-                                                <v-btn
-                                                    icon
-                                                    v-bind="attrs"
-                                                    v-on="on"
-                                                    color="error"
-                                                    @click.stop="cancel_edit()"
-                                                >
-                                                    <v-icon>
-                                                        mdi-close-box
-                                                    </v-icon>
-                                                </v-btn>
-                                            </template>
-                                            <div class="text-center">
-                                                Cancel Changes
-                                            </div>
-                                        </v-tooltip>
-                                        <v-tooltip bottom>
-                                            <template
-                                                v-slot:activator="{ on, attrs }"
-                                            >
-                                                <v-btn
-                                                    icon
-                                                    v-bind="attrs"
-                                                    v-on="on"
-                                                    color="success"
-                                                    :disabled="
-                                                        !isJsonValid ||
-                                                        !made_changes
-                                                    "
-                                                    @click.stop="save_data()"
-                                                >
-                                                    <v-icon>
-                                                        mdi-content-save
-                                                    </v-icon>
-                                                </v-btn>
-                                            </template>
-                                            <div class="text-center">
-                                                Save Changes
-                                            </div>
-                                        </v-tooltip>
-                                    </div>
-                                    <v-tooltip bottom v-else>
-                                        <template
-                                            v-slot:activator="{ on, attrs }"
-                                        >
-                                            <v-btn
-                                                icon
-                                                v-bind="attrs"
-                                                v-on="on"
-                                                color="secondary"
-                                                @click.stop="start_edit()"
-                                            >
-                                                <v-icon>
-                                                    mdi-lead-pencil
-                                                </v-icon>
-                                            </v-btn>
-                                        </template>
-                                        <div class="text-center">
-                                            Edit image properties
-                                        </div>
-                                    </v-tooltip>
-                                </v-col>
-                            </v-row>
-                        </v-toolbar>
-                        <v-card-text>
-                            <div v-if="edit_mode" class="pt-5">
-                                <v-textarea
-                                    filled
-                                    no-resize
-                                    rows="20"
-                                    row-height="20"
-                                    v-model="data_string"
-                                    :error="!isJsonValid"
-                                    :error-messages="
-                                        !isJsonValid ? 'Invalid JSON' : ''
-                                    "
-                                />
-                            </div>
-                            <div v-else>
-                                <v-row>
-                                    <v-col>
-                                        <v-list>
-                                            <v-list-item
-                                                v-for="(
-                                                    value, key
-                                                ) in item.data"
-                                                :key="key"
-                                            >
-                                                <v-list-item-content>
-                                                    <v-list-item-subtitle
-                                                        class="text-button font-weight-medium"
-                                                        >{{
-                                                            key
-                                                        }}</v-list-item-subtitle
-                                                    >
-                                                    <v-list-item-title>
-                                                        <pre>{{
-                                                            format_metadata(
-                                                                value
-                                                            )
-                                                        }}</pre>
-                                                    </v-list-item-title>
-                                                </v-list-item-content>
-                                            </v-list-item>
-                                        </v-list>
-                                    </v-col>
-                                </v-row>
-                            </div>
-                        </v-card-text>
-                    </v-card>
+                    <ImageDataEditor
+                        :json="item.data"
+                        :textarea_row="10"
+                        title="Editable Image Data"
+                        @save-data="save_data"
+                    />
                 </v-col>
             </v-row>
         </v-card-text>
@@ -214,17 +95,18 @@
 
 <script>
 const { VUE_APP_IMAGE_STORAGE_API_URL } = process.env
+import ImageDataEditor from '../components/ImageDataEditor.vue'
 
 export default {
     name: 'Images',
+    components: {
+        ImageDataEditor,
+    },
     data() {
         return {
             loading: false,
             deleting: false,
             item: null,
-            panel: [0, 1],
-            edit_mode: false,
-            data_string: '',
         }
     },
     mounted() {
@@ -241,27 +123,6 @@ export default {
         time_formatted() {
             const date = new Date(this.item.time)
             return date.toLocaleString('ja-JP')
-        },
-        isJsonValid() {
-            try {
-                JSON.parse(this.data_string)
-            } catch (e) {
-                return false
-            }
-            return true
-        },
-        made_changes() {
-            return (
-                JSON.stringify(this.item.data) !==
-                JSON.stringify(JSON.parse(this.data_string))
-            )
-        },
-    },
-    watch: {
-        data_string(newVal) {
-            if (newVal.trim() === '') {
-                this.data_string = '{}'
-            }
         },
     },
     methods: {
@@ -306,34 +167,17 @@ export default {
                 return data
             }
         },
-        start_edit() {
-            this.edit_mode = true
-            if (this.item.data)
-                this.data_string = this.format_metadata(this.item.data)
-        },
-        save_data() {
-            if (!this.isJsonValid) return
-            if (!confirm('Save changes?')) return
+        save_data(data) {
             const route = `/images/${this.image_id}`
-            let data = JSON.parse(this.data_string)
             this.axios
-                .put(route, { ...data })
+                .put(route, data)
                 .then(() => {
                     this.get_item()
-                    this.reset_changes()
                 })
                 .catch((error) => {
                     alert('Failed to delete data')
                     console.error(error)
                 })
-        },
-        cancel_edit() {
-            if (!confirm('Abandon changes?')) return
-            this.reset_changes()
-        },
-        reset_changes() {
-            this.edit_mode = false
-            this.data_string = ''
         },
     },
 }
