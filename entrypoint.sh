2d46caf2a7a858fe74afa1be03915b9d1fa9f564
Leah Joy Ishiguro <leah_ishiguro@jtekt.co.jp>
2025-11-18 14:38:14 +0900

    Fix entry point script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ ROOT_DIR=/app
 
 # Replace env vars in files served by NGINX
 echo "Replacing environment variables"
-for file in $ROOT_DIR/js/*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
+for file in $ROOT_DIR/assets/*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
 do
   echo "Processing $file ...";
 
